880c2323c00a328e6ae8ebc2a8e85d8155a77f0c
qrlk <hi@qrlk.me>
2020-08-08 07:45:35 +0300

    Load defaults in background + show flags added

--- background.js
+++ background.js
@@ -5,49 +5,52 @@ function getUserId() {
     }))), userId
 }
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, true); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
+
+var defaults = {
+    "trust": 70,
+    "accept": 70,
+    "sb": false,
+    "secret": null,
+    "name": "None",
+    "enable": true,
+    "show_flags": true,
+    "uuid": getUserId(),
+    "love": {
+        "y1": false,
+        "y2": false,
+        "a1": false,
+        "a2": false
+    },
+    "fine": {
+        "y1": true,
+        "y2": false,
+        "a1": false,
+        "a2": false
+    },
+    "hate": {
+        "y1": true,
+        "y2": false,
+        "a1": false,
+        "a2": false
+    }
+}
+
+chrome.storage.sync.get(defaults, function (result) {
+    chrome.storage.sync.set(result)
+});
+
 
 //why not?
 chrome.runtime.setUninstallURL("https://karma.adwhore.net:47976/onUnInstall")
 // Check whether new version is installed
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason === "install") {
-        var defaults = {
-            "trust": 70,
-            "accept": 70,
-            "sb": false,
-            "secret": null,
-            "name": "None",
-            "enable": true,
-            "uuid": getUserId(),
-            "love": {
-                "y1": false,
-                "y2": false,
-                "a1": false,
-                "a2": false
-            },
-            "fine": {
-                "y1": true,
-                "y2": false,
-                "a1": false,
-                "a2": false
-            },
-            "hate": {
-                "y1": true,
-                "y2": false,
-                "a1": false,
-                "a2": false
-            }
-        }
-        chrome.storage.sync.get(defaults, function (result) {
-            chrome.storage.sync.set(result)
-        });
 
         httpGet("https://karma.adwhore.net:47976/onInstall")
 
