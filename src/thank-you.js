9409a7ded084c181f826c907ac9f6265c450e62d
Fyodor Kurlyuk <hi@qrlk.me>
2020-08-24 16:14:07 +0300

    Fixed #14

--- src/thank-you.js
+++ src/thank-you.js
@@ -1,24 +1,3 @@
-chrome.storage.sync.get(["uuid", "secret"], function (result) {
-        if (result["secret"] == null) {
-            $.ajax
-            ({
-                url: "https://karma.adwhore.net:47976/addNewUser",
-                type: "POST",
-                data: JSON.stringify({"uuid": result["uuid"]}),
-                contentType: 'application/json',
-                success: function (data) {
-                    chrome.storage.sync.set({"secret": data["secret"], "name": data["name"]});
-                    //alert("Registered\n"+JSON.stringify(data));
-                },
-                error: function (s, status, error) {
-                    alert('error\n' + status + '\n' + error);
-                }
-            })
-        }
-    }
-)
-
-
 function selectSide(id) {
     chrome.storage.sync.get(["secret"], function (result) {
             if (result["secret"] != null) {
