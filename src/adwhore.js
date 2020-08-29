db2cc6455db928d949b717dfeda063024380e002
Fyodor Kurlyuk <hi@qrlk.me>
2020-08-29 17:11:10 +0300

    Using different regex to get vID

--- src/adwhore.js
+++ src/adwhore.js
@@ -1698,15 +1698,8 @@ function disableStage1() {
 }
 
 function getYouTubeID(url) {
-    var ID = '';
-    url = url.replace(/([><])/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-    if (url[2] !== undefined) {
-        ID = url[2].split(/[^0-9a-z_\-]/i);
-        ID = ID[0];
-    } else {
-        ID = url;
-    }
-    return ID;
+    const arr = url.split(/(vi\/|v%3D|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    return 'undefined' !== arr[2] ? arr[2].split(/[^\w-]/i)[0] : arr[0];
 }
 
 function getChannelID(url) {
