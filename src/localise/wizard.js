54d6fd2a2c109cd5c0dcccdb19b9c3a0be450a05
Fyodor Kurlyuk <hi@qrlk.me>
2020-10-22 09:29:53 +0300

    Updated the wizard

--- src/localise/wizard.js
+++ src/localise/wizard.js
@@ -6,6 +6,11 @@ function init() {
     for (const x of Array(50).keys()) {
         setChildTextNode("w"+x, chrome.i18n.getMessage("w"+x))
     }
+    for (const x of Array(15).keys()) {
+        console.log()
+        setChildTextNode("ww"+x, chrome.i18n.getMessage("ww"+x))
+    }
+
 }
 
 init();
