aa5b39cc084e4045d115c26ec20921b1cf395c57
qrlk <hi@qrlk.me>
2022-05-25 16:39:51 +0700

    slightly update wizard text

--- src/localise/wizard.js
+++ src/localise/wizard.js
@@ -7,7 +7,8 @@ function init() {
         setChildTextNode("w"+x, chrome.i18n.getMessage("w"+x))
     }
     for (const x of Array(15).keys()) {
-        setChildTextNode("ww"+x, chrome.i18n.getMessage("ww"+x))
+        if (x !== 4)
+            setChildTextNode("ww"+x, chrome.i18n.getMessage("ww"+x))
     }
 
 }
